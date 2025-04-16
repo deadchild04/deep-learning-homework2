40cbfab87d1943eef0654fcdac8673a6f23e8362
deadchild04 <2413148367@qq.com>
2025-04-16 21:47:01 +0800

    Add files via upload
    使用HTML的表单实现在某一日期下单独查看某一类型的新闻

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect,url_for
 from crawler import NewsCrawler,Newsanalysis
 
 app = Flask(__name__)
 
-time=''
+analysis=None
 
 def category_data(count_category):
     lt=[]
@@ -21,7 +21,8 @@ def get_all_news(url='https://www.chinanews.com/scroll-news/news1.html'):
 @app.route('/')
 def index():
     news_data = get_all_news()
-    analysis = Newsanalysis(news_data)
+    global analysis
+    analysis= Newsanalysis(news_data)
     analysis.count_category_news()
     return render_template('index.html',analysis=analysis)
 
@@ -29,9 +30,20 @@ def index():
 def get_time():
     time=request.form.get('time')
     news_data = get_all_news(url='https://www.chinanews.com/scroll-news/'+time[0:4]+'/'+time[4:8]+'/'+'news.shtml')
+    global analysis
     analysis = Newsanalysis(news_data)
     analysis.count_category_news()
     return render_template('index.html',analysis=analysis)
 
+@app.route('/process',methods=['POST'])
+def process():
+    category=request.form.get('category')
+    return redirect(url_for('news_category',category=category))
+
+@app.route('/news_category/<category>')
+def news_category(category):
+    data=[analysis.news_list[i] for i in range(len(analysis.news_list)) if analysis.news_list[i]['category']==category]
+    return render_template('news_category.html',data=data,category=category)
+    
 if __name__ == '__main__':
     app.run(debug=True)
